e263c24d1f820fc0cf38fd114da986de5449d420
Анна Малышева <a.malysheva@aamsystems.ru>
2020-04-09 16:34:32 +0300

    Updates

---
+++ precache-manifest.3fb0a0bf4fef0401bfbc742b5fc0aee0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a31a25845913859a8c717c98d7bb15",
+    "url": "/react-howework1/index.html"
+  },
+  {
+    "revision": "68b681fd138acde02cf4",
+    "url": "/react-howework1/static/css/2.d788185a.chunk.css"
+  },
+  {
+    "revision": "5c942fb1d9e47fd01ad7",
+    "url": "/react-howework1/static/css/main.1881e2d1.chunk.css"
+  },
+  {
+    "revision": "68b681fd138acde02cf4",
+    "url": "/react-howework1/static/js/2.e269e35b.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-howework1/static/js/2.e269e35b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c942fb1d9e47fd01ad7",
+    "url": "/react-howework1/static/js/main.94eff6b5.chunk.js"
+  },
+  {
+    "revision": "537a72532d2f3047a336",
+    "url": "/react-howework1/static/js/runtime-main.0e94e0e4.js"
+  }
+]);
